3a06cf840d2f088fe7315f11d4d1794420329e3a
mat.aules <mat.aules>
2016-10-25 17:56:53 +0200

    cosas

--- images_P2/ej23.m
+++ images_P2/ej23.m
@@ -21,16 +21,16 @@ function [] = ej23()
     %c)
     logo = rgb2gray(imread('corals.jpg'));
     logo = imfilter(logo, fspecial('average'));
-    contour_sobel = edge(logo, 'Sobel', 0.2);
-    contour_prewitt = edge(logo, 'Prewitt', 0.2);
+    contour_sobel = edge(logo, 'Sobel', 0.05);
+    contour_prewitt = edge(logo, 'Prewitt', 0.05);
     contour_canny = edge(logo, 'Canny', 0.15);
     contour_laplacian = edge(logo, 'log', 0.01);
     figure
     imshow(contour_sobel);
-    title('Sobel (thresh = 0.2)')
+    title('Sobel (thresh = 0.05)')
     figure
     imshow(contour_prewitt);
-    title('Prewitt (thresh = 0.2)')
+    title('Prewitt (thresh = 0.05)')
     figure
     imshow(contour_canny);
     title('Canny (thresh = 0.15)')
