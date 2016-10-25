4b0952942d8bf322ab5c58a576b3747534aebea2
mat.aules <mat.aules>
2016-10-25 17:23:39 +0200

    :D

--- images_P2/ej23.m
+++ images_P2/ej23.m
@@ -1,8 +1,22 @@
 function [] = ej23()
+close all
     logo = rgb2gray(imread('logo.png'));
-    contours = imcontour(logo);
-    contour_sobel = edge(logo, 'Sobel');
-    contour_prewitt = edge(logo, 'Prewitt');
-    contour_canny = edge(logo, 'Canny');
+    contour_sobel = edge(logo, 'Sobel', 0.2);
+    contour_prewitt = edge(logo, 'Prewitt', 0.2);
+    contour_canny = edge(logo, 'Canny', 0.1);
+    contour_laplacian = edge(logo, 'log', 0.01);
+    figure
+    imshow(contour_sobel);
+    title('Sobel (thresh = 0.2)')
+    figure
+    imshow(contour_prewitt);
+    title('Prewitt (thresh = 0.2)')
+    figure
+    imshow(contour_canny);
+    title('Canny (thresh = 0.1)')
+    figure
+    imshow(contour_laplacian);
+    title('Laplaciano (thresh = 0.01)')
+    
 end
 
