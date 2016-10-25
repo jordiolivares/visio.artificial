37cfd97653b39f3e24b3bf4361e017b47513a43b
mat.aules <mat.aules>
2016-10-25 16:25:56 +0200

    ex 2.2

--- images_P2/ej22.m
+++ images_P2/ej22.m
@@ -1,11 +1,11 @@
 function [ output_args ] = ej22( input_args )
-    gaussian = fspecial('gaussian');
+    close all;
     mean_filt = fspecial('average', 3);
-    image = imread('face.png');
+    image = imread('corals.jpg');
     result_gaussian = image;
     result_mean = image;
     for i = 1 : 100
-        result_gaussian = imfilter(result_gaussian, gaussian);
+        result_gaussian = imgaussfilt(result_gaussian, 0.4);
         result_mean = imfilter(result_mean, mean_filt);
     end
     
@@ -14,9 +14,12 @@ function [ output_args ] = ej22( input_args )
     imshow(result_mean)
     
     % b
-    alt_result = imgaussfilt(image, 4);
+    image = imread('face.png');
+    alt_result = imgaussfilt(image, 10);
     figure
     imshow(alt_result)
+    figure
+    imshow(image - alt_result);
     
 
 end
