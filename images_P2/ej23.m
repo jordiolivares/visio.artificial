5bdf27947af93bf2ab152904903cd2d0b30fab8f
mat.aules <mat.aules>
2016-10-25 17:40:22 +0200

    3b

--- images_P2/ej23.m
+++ images_P2/ej23.m
@@ -1,5 +1,5 @@
 function [] = ej23()
-close all
+    close all
     logo = rgb2gray(imread('logo.png'));
     contour_sobel = edge(logo, 'Sobel', 0.2);
     contour_prewitt = edge(logo, 'Prewitt', 0.2);
@@ -14,6 +14,26 @@ close all
     figure
     imshow(contour_canny);
     title('Canny (thresh = 0.1)')
+    figure
+    imshow(contour_laplacian);
+    title('Laplaciano (thresh = 0.01)')
+    
+    %c)
+    logo = rgb2gray(imread('corals.jpg'));
+    logo = imfilter(logo, fspecial('average'));
+    contour_sobel = edge(logo, 'Sobel', 0.2);
+    contour_prewitt = edge(logo, 'Prewitt', 0.2);
+    contour_canny = edge(logo, 'Canny', 0.15);
+    contour_laplacian = edge(logo, 'log', 0.01);
+    figure
+    imshow(contour_sobel);
+    title('Sobel (thresh = 0.2)')
+    figure
+    imshow(contour_prewitt);
+    title('Prewitt (thresh = 0.2)')
+    figure
+    imshow(contour_canny);
+    title('Canny (thresh = 0.15)')
     figure
     imshow(contour_laplacian);
     title('Laplaciano (thresh = 0.01)')
