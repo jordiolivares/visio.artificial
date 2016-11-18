649237c212b873e597229827c356b9ddb88c8ebf
Christian <solero93@gmail.com>
2016-11-18 14:07:03 +0100

    Doing ...

--- imatges_P3/ej33.m
+++ imatges_P3/ej33.m
@@ -1,81 +1,66 @@
 function [ ] = ej33( )
     %%%%%%%%%%%%% PART 1 %%%%%%%%%%%%%
     
-    loro_rgb = double(imread('loro.png'));
+    loro_rgb = imread('loro.png');
     loro = rgb2gray(loro_rgb);
-    
-    re_loro = reshape(loro, [], 1);
+    re_loro = reshape(im2double(loro), [], 1);
     
     array(:,1) = re_loro;
     array(:,2) = re_loro;
     
-    n=4;
+    [rows, columns] = size(loro);
     
+    n=4;
     [idx, C] = kmeans(array, n);
     
     figure
     subplot(1,4,1)
-    hold on
+    
     for i=1:n
-        cluster = array(idx==i,:);
-        plot(cluster(:,1), cluster(:,2), '.')
-        plot(C(i,1), C(i,2), 'o')
+        array(idx==i, 1) = mean(array(idx==i, 1));
     end
-    title('k=4')
-    %legend('show')
+    imshow(reshape(array(:,1), [rows, columns]));
+    title(strcat('k=', n))
     
     %%%%%%%%%%%%% PART 2 %%%%%%%%%%%%%
-    
-    [rows, columns] = size(loro);
-    
     array2 = zeros(rows*columns, 3);
     
     k=1;
     for row = 1 : rows
       for col = 1 : columns
-        array2(k, 1) = row;
-        array2(k, 2) = col;
+        array2(k, 1) = double(row);
+        array2(k, 2) = double(col);
         k = k + 1;
       end
     end
     array2(:, 3) = re_loro;
+    array2(:, 4) = re_loro;
     
-    m=4;
-    
-    [idx, C] = kmeans(array2, m);
+    [idx, C] = kmeans(array2, n);
     
     subplot(1,4,2)
-    hold on
-    for i=1:m
-        cluster = array2(idx==i, :);
-        plot3(cluster(:,1), cluster(:,2), cluster(:,3), '.')
-        plot3(C(i,1), C(i,2), C(i,3), 'o')
+    for i=1:n
+        array2(idx==i, 3) = mean(array2(idx==i, 3));
     end
-    title('k=4')
-    %legend('show')    
+    imshow(reshape(array2(:, 3), [rows, columns]));
+    title(strcat('k=', n))
 
     %%%%%%%%%%%%% PART 3 %%%%%%%%%%%%%
-    
-    rgb_matrix(:,1) = reshape(loro_rgb(:,:,1), [], 1);
-    rgb_matrix(:,2) = reshape(loro_rgb(:,:,2), [], 1);
-    rgb_matrix(:,3) = reshape(loro_rgb(:,:,3), [], 1);
-    
-    [idx, C] = kmeans(rgb_matrix, 16);
     
     subplot(1,4,3)
     plotPixelDistribution(loro_rgb)
     
+    rgb_matrix(:,1) = reshape(double(loro_rgb(:,:,1)), [], 1);
+    rgb_matrix(:,2) = reshape(double(loro_rgb(:,:,2)), [], 1);
+    rgb_matrix(:,3) = reshape(double(loro_rgb(:,:,3)), [], 1);
+    
+    [idx, C] = kmeans(rgb_matrix, 16);
+    
     subplot(1,4,4)
-    hold on
     for i=1:16
-        cluster = rgb_matrix(idx==i, :);
-        
         rgb_matrix(idx==i, 1) = C(i, 1);
         rgb_matrix(idx==i, 2) = C(i, 2);
         rgb_matrix(idx==i, 3) = C(i, 3);
-        
-        plot3(cluster(:,1), cluster(:,2), cluster(:,3), '.')
-        plot3(C(i,1), C(i,2), C(i,3), 'o')
     end
     title('k=16')
     
@@ -84,7 +69,6 @@ function [ ] = ej33( )
     loro_tmp(:,:,2) = rgb_matrix(:,2);
     loro_tmp(:,:,3) = rgb_matrix(:,3);
     
-    imshow(loro_tmp)
-    
+    plotPixelDistribution(loro_tmp)
 end
 
