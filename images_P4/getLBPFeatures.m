f03aba7a010ccc072091dddb711e118cc2ae70ab
Christian <solero93@gmail.com>
2016-11-24 20:16:51 +0100

    Done part (a) of exercise 4.2

---
+++ images_P4/getLBPFeatures.m
@@ -0,0 +1,8 @@
+function [ texture_feature_vector ] = getLBPFeatures( filename )
+%GETLBPFEATURES Summary of this function goes here
+%   Detailed explanation goes here
+    original_image = imread(filename);
+    image = rgb2gray(original_image);
+    histograms = vl_lbp(single(image), 8);
+    texture_feature_vector = mean(histograms,3)
+end
