4ee56a7b9422d198da4a9433428a4946f7b0f8db
mat.aules <mat.aules>
2016-12-01 20:29:46 +0100

    'Hecho' el 4.1

--- images_P4/ej41.m
+++ images_P4/ej41.m
@@ -1,8 +1,7 @@
 function [ output_args ] = ej41( input_args )
 %EJ41 Summary of this function goes here
 %   Detailed explanation goes here
-    F = makeLMfilters();
-    
+    retrieveKImages('texturesimages/forest/forest_17.jpg', 'texturesimages/forest/', @getFeatures)
 end
 
 
