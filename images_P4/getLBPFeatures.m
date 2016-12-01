146794340d2bbe9def582943fd4f29785a9be49d
Christian <solero93@gmail.com>
2016-12-01 20:00:57 +0100

    made lbpfeatures accept image

--- images_P4/getLBPFeatures.m
+++ images_P4/getLBPFeatures.m
@@ -1,8 +1,7 @@
-function [ texture_feature_vector ] = getLBPFeatures( filename )
+function [ texture_feature_vector ] = getLBPFeatures( image )
 %GETLBPFEATURES Summary of this function goes here
 %   Detailed explanation goes here
-    original_image = imread(filename);
-    image = rgb2gray(original_image);
-    histograms = vl_lbp(single(image), 8);
+    img = rgb2gray(image);
+    histograms = vl_lbp(single(img), 8);
     texture_feature_vector = mean(histograms, 3);
 end
